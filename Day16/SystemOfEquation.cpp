9b56fbfcf6e4237a468106d2d6d370187e2f5ade
Devansh Bamrara <69037067+DevanshBamrara@users.noreply.github.com>
2023-02-17 22:40:57 +0530

    Day 16

---
+++ Day16/SystemOfEquation.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m,c=0;
+    cin>>n>>m;
+    for (int i = 0; i =< n; i++)
+    {
+        for (int j = 0; j =< m; j++)
+        {
+           if((i*i)+j==n && (j*j)+i==m){
+                c++;
+           }
+        }
+        
+    }
+    cout<<c;
+    
+    return 0;
+} 
