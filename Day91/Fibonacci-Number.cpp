d601f50afb4d99107e28b67cc9873a89cccc1c31
Devansh Bamrara <69037067+DevanshBamrara@users.noreply.github.com>
2023-05-03 23:38:58 +0530

    Day 91

---
+++ Day91/Fibonacci-Number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int fib(int num) {
+    if (num == 0)
+    {
+        return 0;
+    }
+    else if (num == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return fib(num - 1) + fib(num - 2);
+    }
+    }
+};
